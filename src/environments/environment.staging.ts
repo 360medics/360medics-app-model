1478094dfd3c63605671669dbb1192fd3ba54e53
Alan <alan.schnegg@epitech.eu>
2019-09-10 16:53:30 +0200

    Little change on logo height

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  jsonFileName: 'data_example',   //Name of the Json files you working with
-  toolPath: ''     //Path to the app tools
+  jsonFileName: 'data_hemato',   //Name of the Json files you working with
+  toolPath: 'https://beta.360medics.com/assets/apps/com.hemato.medical/tools'     //Path to the app tools
 };
