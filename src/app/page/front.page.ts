c32d296e1231506ebf0bab89da07d2415b431d7b
Aurelia <aurelia.besse@viacesi.fr>
2020-03-17 17:02:27 +0100

    création in-app

--- src/app/page/front.page.ts
+++ src/app/page/front.page.ts
@@ -72,7 +72,6 @@ export class FrontPageComponent implements OnInit {
     getAppsList() {
         this.data = this.jsonData.appEntries;
         this.appsList = this.data;
-        this.sortApps();
     }
 
     filterAppsList(e: any) {
