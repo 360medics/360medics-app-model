2db2bd026f5f364fec2f2b24d8714c04f6c361d5
AmelieChevalier <ameliechevalier3006@gmail.com>
2021-06-01 16:15:59 +0200

    change base url for html files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   jsonFileName: 'data_hemato',     //Name of the Json files you working with
-  toolPath: 'https://360medics.com/assets/apps/com.hemato.medical/tools/'   //Path to the app tools
+  toolPath: 'https://tools-beta.360medics.com/com.hemato.medical/tools/'   //Path to the app tools
 };
